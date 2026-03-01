281c6cf120c26dfb7a44be7fda84e49300e8790c
Tiny P Christhudas <tinypchristhudas@gmail.com>
2026-03-01 19:03:06 +0530

    conversion of app to async compeleted"

--- database.py
+++ database.py
@@ -1,31 +1,36 @@
 ## database.py
-from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./blog.db"
+SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./blog.db"
 # . is the current directory
 
-engine = create_engine(
+engine = create_async_engine(
     SQLALCHEMY_DATABASE_URL,
     connect_args={"check_same_thread": False},
 )
 # sqllite generally allows only one thread, but fastapi handles multiple requests across threads - you wouldn't need to do this for postgres of mysql
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+AsyncSessionLocal = async_sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False # prevents issue with expired objects after a commit.
+)
 # sessionlocal = factory that creates a database session
 # sesssion = transaction with the database, each request gets its own session
 # we want to control when the changes are committed, that's why autoflush=False, autocommit=False.
 
-
+# expire_on_commit - when an object expired sqlalchemy would normally try to reload it lazily
+# but lazy loading doesn't work in async
 
 
 class Base(DeclarativeBase):
     pass
 
 
-def get_db():
-    with SessionLocal() as db:
-        yield db
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
 # the dependency function that provides sessions to our routes,
 # this is a generator using this yeild db
 # with statement makes the session works like a context manager. kind of like opening a file
