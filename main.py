d643f6039b647e007a5d2c7c3c483ca81c96c12e
Tiny P Christhudas <tinypchristhudas@gmail.com>
2026-03-13 21:48:58 +0530

    posts fetched in the descding order in the query

--- main.py
+++ main.py
@@ -47,7 +47,9 @@ app.include_router(posts.router, prefix="/api/posts", tags=["posts"])
 @app.get("/", include_in_schema=False, name="home")
 @app.get("/posts", include_in_schema=False, name="posts")
 async def home(request: Request, db: Annotated[AsyncSession, Depends(get_db)]):
-    result = await db.execute(select(models.Post).options(selectinload(models.Post.author)))
+    result = await db.execute(select(models.Post)
+                              .options(selectinload(models.Post.author))
+                              .order_by(models.Post.date_posted.desc()))
     posts = result.scalars().all()
     return templates.TemplateResponse(
         request, "home.html",
@@ -76,7 +78,9 @@ async def user_posts_page(
     user_id: int,
     db: Annotated[AsyncSession, Depends(get_db)],
 ):
-    result = await db.execute(select(models.User).where(models.User.id == user_id))
+    result = await db.execute(select(models.User)
+                              .where(models.User.id == user_id)
+                              )
     user = result.scalars().first()
     if not user:
         raise HTTPException(
@@ -86,7 +90,8 @@ async def user_posts_page(
     result = await db.execute(
         select(models.Post)
         .options(selectinload(models.Post.author))
-        .where(models.Post.user_id == user_id),
+        .where(models.Post.user_id == user_id)
+        .order_by(models.Post.date_posted.desc()),
     )
     posts = result.scalars().all()
     return templates.TemplateResponse(
