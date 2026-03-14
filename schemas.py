3fdbfcecb40169c33114c50ea921db7eb0b8e53d
Tiny P Christhudas <tinypchristhudas@gmail.com>
2026-03-14 19:41:50 +0530

    authentication, registration and login with jwt, password hasing with argon complete

--- schemas.py
+++ schemas.py
@@ -10,14 +10,18 @@ class UserBase(BaseModel):
 
 
 class UserCreate(UserBase):
-    pass
+    password: str = Field(min_length=8)
 
-class UserResponse(UserBase):
+class UserPublic(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
     id: int
+    username: str
     image_file: str | None
     image_path: str
+
+class UserPrivate(UserPublic):
+    email: EmailStr
 
 class UserUpdate(BaseModel):
     username: str | None = Field(default=None, min_length=1, max_length=50)
@@ -25,7 +29,9 @@ class UserUpdate(BaseModel):
     image_file: str | None = Field(default=None, min_length=1, max_length=200)
 
 
-
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
 
 #it looks like data classes, but pydantic uses these type hints to validate data at runtime.
@@ -55,4 +61,4 @@ class PostResponse(PostBase):
     id: int # for database models and api responses, using id is basically the standard convension, this id is scope to the class, won't conflict with the global
     user_id: int
     date_posted: datetime # in memory data, when database it would be datetime
-    author: UserResponse
+    author: UserPublic
